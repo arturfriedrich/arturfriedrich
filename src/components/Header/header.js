f62f099f50a25e7aa6c3e78776ea9c5a488eef82
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-02-15 13:50:47 +0100

    Update header.js

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import * as ROUTES from "../../assets/js/routes"
+import "header.css"
 
 export default function Header() {
     return (
