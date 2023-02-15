2d0648798001623ffcd59446b26d4eb25c9558b0
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-02-15 13:18:04 +0100

    created folders and inital files

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ function App() {
 				<Route exact path="/">
 					<Header />
 					<About />
+					<Footer />
+				</Route>
+				<Route path="/aboutme">
+					<Header />
+					<About />
+					<Footer />
 				</Route>
 				<Route path="/projects">
 					<Header />
 					<Projects />
+					<Footer />
 				</Route>
 				<Route path="/contact">
 					<Header />
 					<Contact />
+					<Footer />
 				</Route>
 			</Switch>
 		</Router>
