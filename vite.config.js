9913871d805659bfa01fd0ffe8c0ea5321659e82
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2026-02-25 12:17:03 +0100

    settings

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        api: 'modern-compiler'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'build',
+    publicDir: 'public'
+  }
+})
